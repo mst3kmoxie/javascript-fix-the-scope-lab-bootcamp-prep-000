42806c4780995a6a63070bb7cf5398bb65fdd813
Dawn Carroll <dsc254@gmail.com>
2018-01-16 14:08:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  n = parseInt(n,10);
+  
+  return n + two
 }
 
 var funkyFunction = function() {
